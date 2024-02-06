2abec4c1e257773b9c4a5193f198c563b2a5897e
sahil3982 <113926133+Sahil3982@users.noreply.github.com>
2024-02-06 14:22:38 +0530

    Jan : 14:22

--- ReactForm/src/App.jsx
+++ ReactForm/src/App.jsx
@@ -8,10 +8,48 @@ const App = () => {
     setInputValue(event.target.value);
   }
 
+  const[showbtn,setShowbtn] = useState(false);
+  const[titile,setTitle] = useState([
+    {
+      id:1,
+      name:"SAHIL",
+      age : 22
+    }
+    ,
+    {
+      id:2,
+      name:"sataym",
+      age: 21
+    }
+    ,
+    {
+      id : 3,
+      name : "Pawsn",
+      age: 23
+    }
+  ])
+
+   
 
   return (
    <>
-   <Useref />
+   <div>
+
+      <div> <button>{titile.map( item =>{
+        return (
+          <>
+          <div>{item.name}</div>
+          <div>{item.age}</div>
+          </>
+        )
+      } )}</button></div>
+
+   </div>
+
+{/* {showbtn ? <button>Show me</button> : "Nothing"} */}
+{showbtn && <button>Show me</button>}
+
+   {/* <Useref /> */}
     {/* <form>
     <input type='text'  value={inputValue} onChange={handleInputChange} />
 
