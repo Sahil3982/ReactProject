a4f8ee7a5dca1ff78d892b6d3d0e9d4c7529ebbc
Sahil3982 <sahilpatel3982@gmail.com>
2024-04-10 22:43:15 +0530

    Apr'10

--- DiceGame/src/Components/GameBorad.jsx
+++ DiceGame/src/Components/GameBorad.jsx
@@ -1,46 +1,114 @@
 // import React from 'react'
-import '../App.css'
-import dice1 from '../assets/dice1.svg'
-import dice2 from '../assets/dice2.svg'
-import dice3 from '../assets/dice3.svg'
-import dice4 from '../assets/dice4.svg'
-import dice5 from '../assets/dice5.svg'
-import dice6 from '../assets/dice6.svg'
+import { useState } from "react";
+import "../App.css";
+
 const GameBorad = () => {
+  const [number, setNumber] = useState(3);
+
+  function RandomNumberGenerate(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    setNumber(num);
+  }
+
+  const showRules = () => {};
   return (
     <>
-    <section style={{display: 'flex', margin:'30px', justifyContent:'space-between '}}>
-        <article style={{display: 'flex', flexDirection:'column' ,justifyContent : 'center' , alignItems : 'center'}}>
-            <span style={{fontSize:'70px'}}>0</span>
-            <span>Total Score</span>
+      <section
+        style={{
+          display: "flex",
+          margin: "30px",
+          justifyContent: "space-between",
+          flexWrap: "wrap",
+        }}
+      >
+        <article
+          style={{ flex: "1", textAlign: "center", marginBottom: "20px" }}
+        >
+          <span style={{ fontSize: "70px", fontWeight: "2000px" }}>0</span>
+          <br />
+          <strong>Total Score</strong>
         </article>
-       <article style={{display: 'flex', flexDirection:'column', gap:'20px'}}>
-       <div style={{ display:'flex', gap: '5px'}}>
-            <button className='my-button' style={{padding:'1rem'}}>1</button>
-            <button className='my-button' style={{padding:'1rem'}}>2</button>
-            <button className='my-button' style={{padding:'1rem'}}>3</button>
-            <button className='my-button' style={{padding:'1rem'}}>4</button>
-            <button className='my-button' style={{padding:'1rem'}}>5</button>
-            <button className='my-button' style={{padding:'1rem'}}>6</button>
-        </div>
-        <div>
-            <b style={{float:'right'}}>Select Number</b>
+        <article
+          style={{
+            flex: "1",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{ display: "flex", gap: "5px", justifyContent: "center" }}
+          >
+            <button className="my-button" style={{ padding: "1rem" }}>
+              1
+            </button>
+            <button className="my-button" style={{ padding: "1rem" }}>
+              2
+            </button>
+            <button className="my-button" style={{ padding: "1rem" }}>
+              3
+            </button>
+            <button className="my-button" style={{ padding: "1rem" }}>
+              4
+            </button>
+            <button className="my-button" style={{ padding: "1rem" }}>
+              5
+            </button>
+            <button className="my-button" style={{ padding: "1rem" }}>
+              6
+            </button>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <strong>Select Number</strong>
+          </div>
+        </article>
+      </section>
 
-        </div>
-       </article>
-    </section>
+      <section>
+        <center>
+          <img
+            src={`/Images/dice${number}.svg`}
+            style={{ width: "150px" }}
+            onClick={() => RandomNumberGenerate(1, 6)}
+          />
+        </center>
+      </section>
 
-    <section style={{ display:'flex' ,alignItems:'center' ,justifyContent:'center', bottom:'200px'}}>
-    <img src={dice1} style={{width : '200px', position:'absolute' }}/>
-    <img src={dice2} style={{width : '200px' , position:'absolute'  }}/>
-    <img src={dice3} style={{width : '200px', position:'absolute'  }}/>
-    <img src={dice4} style={{width : '200px', position:'absolute'  }}/>
-    <img src={dice5} style={{width : '200px', position:'absolute'  }}/>
-    <img src={dice6} style={{width : '200px', position:'absolute'  }}/>
-
-    </section>
+      <section>
+        <center>
+          <h3>Click on the Dice to roll</h3>
+          <button
+            style={{
+              padding: "5px",
+              paddingLeft: "70px",
+              paddingRight: "70px",
+              marginBottom: "20px",
+              marginTop: "20px",
+            }}
+          >
+            Reset
+          </button>
+          <br />
+          <button
+            onClick={showRules}
+            style={{
+              padding: "5px",
+              paddingLeft: "50px",
+              paddingRight: "50px",
+              backgroundColor: "black",
+              color: "white",
+            }}
+          >
+            Show Rules
+          </button>
+        </center>
+      </section>
+      <section>
+        <div></div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default GameBorad
+export default GameBorad;
